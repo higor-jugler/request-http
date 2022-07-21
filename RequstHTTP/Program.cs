241c0fa773fdaf9f1d931b71d6d696b301d11a70
Higor Jugler <higorjugler@hotmail.com>
2022-07-21 15:05:02 -0300

    Definido metodos de leitura da api

--- RequstHTTP/Program.cs
+++ RequstHTTP/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+
+var req = WebRequest.Create("https://jsonplaceholder.typicode.com/todos");
+req.Method = "GET";
+
+using(var resp = req.GetResponse())
+{
+    var stream = resp.GetResponseStream();
+    StreamReader leitor = new StreamReader(stream);
+    object dados = leitor.ReadToEnd();
+
+    Console.WriteLine(dados.ToString());
+
+    stream.Close();
+    resp.Close();
+}
+
+Console.ReadLine();
