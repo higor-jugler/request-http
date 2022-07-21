9072391961c8e44534635e52b4d5c9c033e30fc7
Higor Jugler <higorjugler@hotmail.com>
2022-07-21 15:31:51 -0300

    Formantando dados com JSON

--- RequstHTTP/Program.cs
+++ RequstHTTP/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net;
+using Newtonsoft.Json;
+using RequstHTTP;
 
 var req = WebRequest.Create("https://jsonplaceholder.typicode.com/todos");
 req.Method = "GET";
@@ -9,7 +11,16 @@ using(var resp = req.GetResponse())
     StreamReader leitor = new StreamReader(stream);
     object dados = leitor.ReadToEnd();
 
-    Console.WriteLine(dados.ToString());
+    //Console.WriteLine(dados.ToString());
+
+    List<Tarefa> tarefas = JsonConvert.DeserializeObject<List<Tarefa>>(dados.ToString());
+
+    //Console.WriteLine(tarefas);
+
+    foreach(Tarefa tarefa in tarefas)
+    {
+        tarefa.Exibir();
+    }
 
     stream.Close();
     resp.Close();
---
+++ RequstHTTP/Tarefa.cs
@@ -0,0 +1,21 @@
+﻿
+namespace RequstHTTP
+{
+    internal class Tarefa
+    {
+        public int userId;
+        public int id;
+        public string title;
+        public bool completed;
+
+        public void Exibir()
+        {
+            Console.WriteLine($"\nUser ID: {userId}");
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Completed? {completed}\n");
+            Console.WriteLine("=========================");
+        }
+
+    }
+}
